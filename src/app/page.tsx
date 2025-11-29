f6d8e9240a8b7662745487f674b7543c16bcac83
Lukas <r.yusifli@yahoo.com>
2025-11-29 22:45:34 +0100

    Add Home component and styles for Easy Build Studio link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import styles from "./home.module.css"
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <a href="https://www.roblox.com/communities/36026412/EZBuild-Studio#!/about" className={styles.ezbuild}>Easy Build Studio</a>
+    </div>
   );
 }
